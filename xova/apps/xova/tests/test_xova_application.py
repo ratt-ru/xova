7e0d46aade5a399b4b1069726a552272ef709886
Simon Perkins <simon.perkins@gmail.com>
2020-04-23 12:45:00 +0200

    Remove old test case

--- xova/apps/xova/tests/test_xova_application.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-from xova.apps.xova.app import Application
-
-
-@pytest.mark.parametrize("ms", ["/home/sperkins/data/WSRT_multiple.MS_p0"])
-def test_xova_application(ms):
-    Application([ms]).execute()
