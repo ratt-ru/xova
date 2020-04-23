165f9455e5641a02d6a7b058b43d15ddefe7271c
Simon Perkins <simon.perkins@gmail.com>
2020-04-23 13:30:28 +0200

    Add a field selection test

--- xova/apps/xova/tests/test_application.py
+++ xova/apps/xova/tests/test_application.py
@@ -7,3 +7,7 @@ from xova.apps.xova.app import Application
 def test_application():
     ms = test_ms()
     Application([ms]).execute()
+
+def test_field_selection():
+    ms = test_ms()
+    Application([ms, "-f 0"]).execute()
