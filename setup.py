54c108230af7030df105e517503a0b9496ed9edd
Simon Perkins <simon.perkins@gmail.com>
2020-07-04 13:44:28 +0200

    Add python 3.8 classifier

--- setup.py
+++ setup.py
@@ -43,6 +43,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     description="Measurement Set Averager",
     entry_points={
