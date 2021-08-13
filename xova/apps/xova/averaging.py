Misc fixes for bda - Fixes single channel bda averaging - Fixes minimum number of channels per bin larger than available channels - Significantly improves UVW recomputation performance on large sets

xova/apps/xova/averaging.py
@@ -487,10 +487,11 @@ def ddid_and_spw_factory(chan_freqs, chan_widths,
                                         axis=0)
 
     # Sanity checks
-    if not all(np.all(np.diff(cf) > 0.0) for cf in chan_freqs):
+    if not (all(cf.size >= 1 for cf in chan_freqs) and all(cw.size >= 1 for cw in chan_widths)):
+        raise ValueError("No SPW may be empty")
+    if not all(np.all(np.diff(cf) > 0.0 if cf.size > 1 else [True]) for cf in chan_freqs):
         raise ValueError("Decreasing CHAN_FREQ unsupported")
-
-    if not all(np.all(cw[0] == c for c in cw) for cw in chan_widths):
+    if not all(np.all(cw[0] == c for c in (cw if cw.size > 1 else cw.reshape(1))) for cw in chan_widths):
         raise ValueError("Heterogenous CHAN_WIDTH unsupported")
 
     rowid = 0
@@ -503,10 +504,12 @@ def ddid_and_spw_factory(chan_freqs, chan_widths,
 
     # Generate SPW's for each channelisation
     for spw, nchan in uspw_chan_map:
-        start = chan_freqs[spw][0] - chan_widths[spw][0] / 2
-        end = chan_freqs[spw][-1] + chan_widths[spw][-1] / 2
+        cf = chan_freqs[spw] if chan_freqs[spw].size > 1 else chan_freqs[spw].reshape(1)
+        cw = chan_widths[spw] if chan_widths[spw].size > 1 else chan_widths[spw].reshape(1)
+        start = cf[0] - cw[0] / 2
+        end = cf[-1] + cw[-1] / 2
 
-        bandwidth = chan_widths[spw].sum()  # Maybe TOTAL_BANDWIDTH?
+        bandwidth = cw.sum()  # Maybe TOTAL_BANDWIDTH?
         cw = np.full(nchan, bandwidth / nchan)
         cf = np.linspace(start + cw[0] / 2,
                          end - cw[-1] / 2,

xova/apps/xova/fixvis.py
@@ -9,13 +9,58 @@ try:
     from loguru import logger
 except:
     from logging import log as logger
-from numba import jit
+from numba import jit, prange
 
-def antenna_indicies(na, auto_correlations=True):
-    """ Compute base antenna pairs from baseline index """
-    k = 0 if auto_correlations == True else 1
-    ant1, ant2 = np.triu_indices(na, k)
-    return np.stack([ant1, ant2], axis=1)
+class progress:
+    def __init__(self, max=100):
+        self.prevprintval = 0
+        self.value = 0
+        self.max = max
+        self.completed = False
+        print(f"{self.prevprintval}...", end="", flush=True)
+
+    def increment(self):
+        if self.completed: return
+        self.value += 1
+        if self.value * 100 / self.max - self.prevprintval >= 10:
+            self.prevprintval += 10
+            print(f"{self.prevprintval}..." if self.prevprintval < 100 else
+                  f"{self.prevprintval}",
+                  end=""  if self.prevprintval < 100 else "\n", 
+                  flush=True)
+        if self.prevprintval == 100:
+            self.completed = True
+
+def baseline_index(a1, a2, no_antennae):
+  """
+   Computes unique index of a baseline given antenna 1 and antenna 2
+   (zero indexed) as input. The arrays may or may not contain
+   auto-correlations.
+
+   There is a quadratic series expression relating a1 and a2
+   to a unique baseline index(can be found by the double difference
+   method)
+
+   Let slow_varying_index be S = min(a1, a2). The goal is to find
+   the number of fast varying terms. As the slow
+   varying terms increase these get fewer and fewer, because
+   we only consider unique baselines and not the conjugate
+   baselines)
+   B = (-S ^ 2 + 2 * S *  # Ant + S) / 2 + diff between the
+   slowest and fastest varying antenna
+
+  :param a1: array of ANTENNA_1 ids
+  :param a2: array of ANTENNA_2 ids
+  :param no_antennae: number of antennae in the array
+  :return: array of baseline ids
+  """
+  if a1.shape != a2.shape:
+    raise ValueError("a1 and a2 must have the same shape!")
+
+  slow_index = np.min(np.array([a1, a2]), axis=0)
+
+  return (slow_index * (-slow_index + (2 * no_antennae + 1))) // 2 + \
+         np.abs(a1 - a2)
 
 def dense2sparce_uvw(a1, a2, time, ddid, padded_uvw):
     """
@@ -37,28 +82,20 @@ def dense2sparce_uvw(a1, a2, time, ddid, padded_uvw):
     nbl = na * (na - 1) // 2 + na
     unique_time = np.unique(time)
     ntime = unique_time.size
-    antindices = antenna_indicies(na, auto_correlations=True)
+    antindices = np.stack(np.triu_indices(na, 0),
+                          axis=1)
     padded_time = unique_time.repeat(nbl) 
     padded_a1 = np.tile(antindices[:, 0], (1, ntime)).ravel()
     padded_a2 = np.tile(antindices[:, 1], (1, ntime)).ravel()
-
+    padded_bl = baseline_index(padded_a1, padded_a2, na)
     new_uvw = np.zeros((a1.size, 3), dtype=padded_uvw.dtype)
-    ala = np.logical_and
-    alo = np.logical_or
-
+    outbl = baseline_index(a1, a2, na)
+    p = progress(a1.size)
     for outrow in range(a1.size):
-        outt = time[outrow]
-        outa1 = a1[outrow]
-        outa2 = a2[outrow]
+        p.increment()
+        lookupt = np.argwhere(unique_time == time[outrow])
         # note: uvw same for all ddid (in m)
-        intsel = padded_time == outt
-        inblsel = alo(ala(padded_a1 == outa1,
-                          padded_a2 == outa2),
-                      ala(padded_a2 == outa1,
-                          padded_a1 == outa2))
-        insel = ala(intsel, inblsel)
-        assert np.sum(insel) == 1
-        new_uvw[outrow][:] = padded_uvw[insel]
+        new_uvw[outrow][:] = padded_uvw[lookupt * nbl + outbl[outrow], :]
 
     return new_uvw
 
@@ -102,7 +139,8 @@ def synthesize_uvw(station_ECEF, time, a1, a2,
     # keep a full uvw array for all antennae - including those
     # dropped by previous calibration and CASA splitting
     padded_uvw = np.zeros((ntime * nbl, 3), dtype=np.float64)
-    antindices = antenna_indicies(na, auto_correlations=True)
+    antindices = np.stack(np.triu_indices(na, 0),
+                          axis=1)
     padded_time = unique_time.repeat(nbl) 
     padded_a1 = np.tile(antindices[:, 0], (1, ntime)).ravel()
     padded_a2 = np.tile(antindices[:, 1], (1, ntime)).ravel()
@@ -121,8 +159,9 @@ def synthesize_uvw(station_ECEF, time, a1, a2,
     dm.do_frame(obs)
     dm.do_frame(refdir)
     dm.do_frame(epoch)
-
+    p = progress(unique_time.size)
     for ti, t in enumerate(unique_time):
+        p.increment()
         epoch = dm.epoch("UT1", quantity(t, "s"))
         dm.do_frame(epoch)
 

xova/apps/xova/tests/test_fixvis.py
@@ -2,7 +2,6 @@
 # noqa: E501
 
 from xova.apps.xova.fixvis import (synthesize_uvw,
-                                   antenna_indicies,
                                    dense2sparce_uvw)
 import numpy as np
 import time
@@ -47,7 +46,8 @@ def test_synthesize_uvw():
     ntime = 50
     padded_a1 = np.empty((nbl), dtype=np.int32)
     padded_a2 = np.empty((nbl), dtype=np.int32)
-    antindies = antenna_indicies(na)
+    antindies = np.stack(np.triu_indices(na, 0),
+                         axis=1)
     for bl in range(nbl):
         blants = antindies[bl]
         padded_a1[bl] = blants[0]
@@ -72,7 +72,8 @@ def test_uvw_sparse():
     ntime = 100
     padded_a1 = np.empty((nbl), dtype=np.int32)
     padded_a2 = np.empty((nbl), dtype=np.int32)
-    antindies = antenna_indicies(na)
+    antindies = np.stack(np.triu_indices(na, 0),
+                         axis=1)
     for bl in range(nbl):
         blants = antindies[bl]
         padded_a1[bl] = blants[0]