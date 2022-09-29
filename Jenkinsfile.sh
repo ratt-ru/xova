1b7761ff7b9ed7fb378c10663981f29cf8709711
bennahugo <bennahugo@gmail.com>
2022-09-29 20:15:36 +0200

    add more complicated bda real data test

--- Jenkinsfile.sh
+++ Jenkinsfile.sh
@@ -19,14 +19,24 @@ TEST_MS_REL=1519747221.subset.ms
 tar xvf $TEST_DATA_DIR/acceptance_test_data.tar.gz -C $TEST_OUTPUT_DIR
 
 # test 3.7
+# basic unit tests
 docker run \
     --rm \
     -v $TEST_OUTPUT_DIR:/testdata \
-    --env TRICOLOUR_TEST_MS=/testdata/$TEST_MS_REL \
     --workdir /code \
     --entrypoint /bin/sh \
     xova.1804.py37:${BUILD_NUMBER} \
     -c "python3 -m pytest --flake8 -s -vvv ."
+
+# acceptance tests
+docker run \
+    --rm \
+    -v $TEST_OUTPUT_DIR:/testdata \
+    --workdir /code \
+    --entrypoint /bin/sh \
+    xova.1804.py37:${BUILD_NUMBER} \
+    -c "xova bda /testdata/$TEST_MS_REL -fov 1.0 -d 0.8 -dc DATA -mc 24 --force -o /testdata/${TEST_MS_REL}.bda"
+
 
 rm $TEST_OUTPUT_DIR/$TEST_MS_REL -rf
 tar xvf $TEST_DATA_DIR/acceptance_test_data.tar.gz -C $TEST_OUTPUT_DIR
@@ -35,8 +45,16 @@ tar xvf $TEST_DATA_DIR/acceptance_test_data.tar.gz -C $TEST_OUTPUT_DIR
 docker run \
     --rm \
     -v $TEST_OUTPUT_DIR:/testdata \
-    --env TRICOLOUR_TEST_MS=/testdata/$TEST_MS_REL \
     --workdir /code \
     --entrypoint /bin/sh \
     xova.2004.py38:${BUILD_NUMBER} \
     -c "python3 -m pytest --flake8 -s -vvv ."
+
+docker run \
+    --rm \
+    -v $TEST_OUTPUT_DIR:/testdata \
+    --workdir /code \
+    --entrypoint /bin/sh \
+    xova.2004.py38:${BUILD_NUMBER} \
+    -c "xova bda /testdata/$TEST_MS_REL -fov 1.0 -d 0.8 -dc DATA -mc 24 --force -o /testdata/${TEST_MS_REL}.bda"
+
